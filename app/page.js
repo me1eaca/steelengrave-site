6a2100fbc244e5b94028dbd97e5c9399017a8258
Cosmin <office@awareconnections.eu>
2025-09-30 20:18:39 +0300

    Initial commit - SteelEngrave site

---
+++ app/page.js
@@ -0,0 +1,8 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Menu, X, Phone, Mail, MapPin, ArrowRight, Zap, Layers, Settings, Wrench, Shield, Clock } from 'lucide-react';
+
+export default function SteelEngraveSite() {
+  // Tot codul din artifact aici
+}
